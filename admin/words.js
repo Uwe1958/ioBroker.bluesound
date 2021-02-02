b1ac16b30e08aa25fa613571cd7d624b63d269d0
Uwe1958 <uwenagel@kabelmail.de>
2021-02-02 17:43:00 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'bluesound adapter settings': {
+		'en': 'Adapter settings for bluesound',
+		'de': 'Adaptereinstellungen für bluesound',
+		'ru': 'Настройки адаптера для bluesound',
+		'pt': 'Configurações do adaptador para bluesound',
+		'nl': 'Adapterinstellingen voor bluesound',
+		'fr': "Paramètres d'adaptateur pour bluesound",
+		'it': "Impostazioni dell'adattatore per bluesound",
+		'es': 'Ajustes del adaptador para bluesound',
+		'pl': 'Ustawienia adaptera dla bluesound',
+		'zh-cn': 'bluesound的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'option1',
+		'ru': 'option1',
+		'pt': 'option1',
+		'nl': 'option1',
+		'fr': 'option1',
+		'it': 'option1',
+		'es': 'option1',
+		'pl': 'option1',
+		'zh-cn': 'option1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'option2',
+		'ru': 'option2',
+		'pt': 'option2',
+		'nl': 'option2',
+		'fr': 'option2',
+		'it': 'option2',
+		'es': 'option2',
+		'pl': 'option2',
+		'zh-cn': 'option2'
+	}
+};
