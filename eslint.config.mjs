bad7e0de79253a8944bce886fab6036e9ec400a6
Uwe1958 <uwenagel@kabelmail.de>
2024-05-22 15:12:26 +0200

    Further eslint modifications

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import pluginJs from '@eslint/js';
 export default [
     {
         languageOptions: { globals: globals.browser },
-        ignorePatterns: ['.prettierrc.js', '**/.eslintrc.js', 'admin/words.js'],
+        ignores: ['.prettierrc.js', '**/.eslintrc.js', 'admin/words.js'],
     },
     pluginJs.configs.recommended,
 ];
