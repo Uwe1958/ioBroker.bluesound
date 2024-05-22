86b1897b9bbea62a30243f0ff1a01e2a7e01343f
Uwe1958 <uwenagel@kabelmail.de>
2024-05-22 15:09:25 +0200

    eslint nodifications

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {
+        languageOptions: { globals: globals.browser },
+        ignorePatterns: ['.prettierrc.js', '**/.eslintrc.js', 'admin/words.js'],
+    },
+    pluginJs.configs.recommended,
 ];
